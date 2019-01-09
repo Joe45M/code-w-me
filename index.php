57979e9828eea08686acb621b258c5ec686a36d6
Joe <joe45moses@gmail.com>
2019-01-09 02:35:25 +0000

    midway

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport"><!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link href="assets/style.css" rel="stylesheet">
+    <title>Let's code!</title>
+</head>
+<body> 
+    <div class="landing-wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <h1>Let's code</h1>
+                    <p>
+                        Hi! I'm Joe. A Website Developer of four years.  <br> <br>
+
+                        When I began learning to code, I felt stuck. There is so much to learn and it can be confusing, especially when you do not have any help. <br> <br>
+
+                        Which is why I want to help. Everyone should be able to learn how to code without feeling lost or scared. You can think of this as a peer-learning program with mentor-like guidance.
+                    </p>
+                </div>
+                <div class="col-sm-7 d-none d-md-block">
+                    <img src="assets/img/coding.svg" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="hdiw-wrap">
+        <div class="container">
+            <div class="row title-row">
+                <div class="col-sm-12">
+                    <h1 class="tc">
+                        How does it work?
+                    </h1>
+                    <p class="tc">
+                        That is your decision to make. 
+                    </p>
+                </div>
+            </div>
+            <div class="row reason-row">
+                <div class="col-sm-6">
+                    <img src="assets/img/peer.svg" class="r" alt="">
+                </div>
+                <div class="col-sm-6">
+                    <h2 class="">
+                        Mentor-mentee style
+                    </h2>
+                    <p class="rt">
+                        A lot of people benefit from one-to-one mentoring. It can be seen as a more tailored experience as well as something more long-term. 
+                        <br><br>
+                        With a one-to-one system, we can set goals and perform custom exercises which are entirely fitted to your learning style, level, and needs. We can create a Google Sheet which tracks your progress and more. 
+                    </p>
+                </div>
+            </div>
+            <div class="row reason-row">
+                <div class="col-sm-6">
+                    <h2 class="">
+                        Peer learning style
+                    </h2>
+                    <p class="rt">
+                        Working in a team to problem solve, learn and meet new people can be fun, flexible and can have a positive impact on your learning experience. 
+                        <br><br>
+                       We want to build a community of people just like you. Eager coders who want to learn, teach and meet new people. Have a coding question? Ask in our <a href="https://discord.gg/KEsbkF">Discord</a>. Want to help others? Answer in our discord. Our servers and chats are supportive, moderated and welcoming.
+                       <br><br>
+                       We encourage questions!
+    
+                    </p>
+                </div>
+                <div class="col-sm-6">
+                    <img src="assets/img/group.svg" class="r" alt="">
+                </div>
+            </div>
+             <div class="row reason-row">
+                <div class="col-sm-6">
+                    <img src="assets/img/msg.svg" class="r" alt="">
+                </div>
+                <div class="col-sm-6">
+                    <h2 class="">
+                        Mix it up
+                    </h2>
+                    <p class="rt">
+                        Want short-term one-to-one mentoring AND to become a part of the peer community? Awesome! We encourage you to come and go as you please.
+                        <br><br>
+                        Want to miss out the mentoring and instead chill with the community? Fabulous! 
+                        <br><br>
+                        Want long-term O-T-O mentoring?  Awesome! 
+                        <br><br>
+                        What I am trying to say is, you can participate at your own pace! :)
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="join-now-wrap">
+        <div class="container">
+            <div class="row col-sm-12">
+                <h1 class="tc">
+                    Want in?
+                </h1>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js">
+    </script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js">
+    </script> 
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js">
+    </script>
+</body>
+</html>
